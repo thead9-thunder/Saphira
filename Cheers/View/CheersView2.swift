f1532871ffa6c68fc09b06a5d3e6b986cfbc2b0a
thead9-thunder <thomas@thunderingdragon.com>
2025-05-23 18:49:56 -0400

    Going to try new navigation strategy

---
+++ Cheers/View/CheersView2.swift
@@ -0,0 +1,25 @@
+//
+//  CheersView2.swift
+//  Cheers
+//
+//  Created by Thomas Headley on 5/23/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct CheersView2: View {
+    var body: some View {
+        NavigationSplitView {
+
+        } detail: {
+
+        }
+    }
+}
+
+extension CheersView2 {
+    enum NavigationDestination: Hashable {
+        case
+    }
+}
